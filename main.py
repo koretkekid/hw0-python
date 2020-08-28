2a555605050f857d03f24ce3bf7d0aef430c8b51
Alex Korteke <ajk6357@psu.edu>
2020-08-28 17:35:21 +0000

    changed spacing and a typo parenthesis

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 celsius = float(input("Enter temperature in celsius: "))
 fahrenheit = (9/5*celsius + 32)
 
-print(f'({celsius}{chr(176)}  in Celsius is equivalent to {fahrenheit}{chr(176)} Fahrenheit')
+print(f'{celsius}{chr(176)} in Celsius is equivalent to {fahrenheit}{chr(176)} Fahrenheit.')
