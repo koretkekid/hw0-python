ec3d452955fc10b007ca628a6653c6e74f211319
Alex Korteke <ajk6357@psu.edu>
2020-08-28 17:32:00 +0000

    put the number and degree

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-celsius = input("Enter temperature in celsius: ")
-celsius = float(celsius)
+celsius = float(input("Enter temperature in celsius: "))
 fahrenheit = (9/5*celsius + 32)
-print(celsius, chr(176) , "in Celsius is equivalent to " , fahrenheit, chr(176) , "Fahrenheit")
+
+print(f'({celsius}{chr(176)}  in Celsius is equivalent to {fahrenheit}{chr(176)} Fahrenheit')
